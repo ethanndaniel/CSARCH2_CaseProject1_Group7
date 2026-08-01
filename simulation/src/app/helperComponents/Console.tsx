d87fc01d469548774007de919dea63d3ffd0b69d
Gabrielle Joei <gjvbactong@gmail.com>
2026-08-01 16:17:59 +0800

    Feat: console

---
+++ simulation/src/app/helperComponents/Console.tsx
@@ -0,0 +1,32 @@
+"use client"
+import React, { useState } from 'react'
+
+const Console = () => {
+    const [logs, setLogs] = useState<string[]>([
+    "Hey gang",
+    "kms",
+  ]); 
+
+  return (
+    <div className="h-48 min-h-[120px] max-h-[40vh] bg-gray-950 border-t border-gray-800 p-4 flex flex-col justify-between">
+
+        <div className="flex justify-between border-t border-gray-800 items-center mb-2 pb-1">
+            <span className="text-gray-300">Console</span>
+            <button onClick={() => setLogs([])}>
+                Clear
+            </button>
+        </div>
+
+        <div className="flex-1 overflow-y-auto space-y-1 text-green-400 whitespace-pre-wrap">
+            {logs.map((log, index) => (
+            <div key={index} className="leading-tight">
+                <span className="text-gray-600 select-none">&gt; </span>
+                {log}
+            </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Console
--- simulation/src/app/page.tsx
+++ simulation/src/app/page.tsx
@@ -2,20 +2,32 @@
 import Image from "next/image";
 import {useState} from "react";
 import Input from "./helperComponents/Input";
+import Console from "./helperComponents/Console";
 
 export default function Home() {
-  const [word,setWord]=useState("");
+  const [word, setWord] = useState("");
+
   return (
-    <div className="min-h-screen w-full bg-white flex justify-center items-center gap-2 py-2">
+    <div className="h-screen w-full bg-white flex justify-center items-center gap-2 py-2">
+
+        {/* Toolbox view */}
         <div className="w-[27vw] min-h-screen bg-[#111844]">
           <Input/>
-        <div>
-    </div>
+        </div>
 
-        </div>
-        <div className="w-[70vw] min-h-screen bg-black">
-              {word}
-        </div>
+        {/* Main View */}
+        <div className="w-[70vw] h-full bg-gray-300 flex flex-col justify-between overflow-hidden">
+
+          {/* Simulator */}
+          <div className="flex-1 p-4 overflow-auto text-white">
+            {word}
+          </div>
+
+          {/* Console */}
+          <div>
+            <Console/>
+          </div>
+      </div> 
     </div>
   );
 }
