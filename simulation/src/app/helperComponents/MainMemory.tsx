4235a3d000dbb9f024a8e319f0bbc8c843f4a3ce
Catherine-Gunita <Catherine_gunita@dlsu.edu.ph>
2026-08-02 18:58:14 +0800

    update on simulator

--- simulation/src/app/helperComponents/MainMemory.tsx
+++ simulation/src/app/helperComponents/MainMemory.tsx
@@ -1,11 +1,10 @@
 
-
-
 export default function MainMemory(){
     return(
-        <div>
+        <div className="w-[15vw] h-[70vh] overflow-auto bg-gray-400 gap-1 p-2 border-1 border-black">
+            <h1 className="text-center text-white font-bold">Main Memory</h1>
             {Array.from({length: 1024}, (_, i) => i + 1).map((num, index) => (
-                <Blocks key={index} num={index+1}/>
+                <Blocks key={index} num={index}/>
             ))}
         </div>
     );
@@ -14,7 +13,7 @@ export default function MainMemory(){
 
 function Blocks({num}:{num:number}){
     return(
-        <div className="bg-blue-500 w-[5vw] h-[5vw] flex justify-center items-center text-white">
+        <div className="bg-blue-500 w-full h-auto flex justify-center items-center text-white border-1 border-black">
             <h1>{num}</h1>
         </div>
     );
--- simulation/src/app/page.tsx
+++ simulation/src/app/page.tsx
@@ -4,6 +4,9 @@ import {useState} from "react";
 import Input from "./helperComponents/Input";
 import Console from "./helperComponents/Console";
 import MainMemory from "./helperComponents/MainMemory";
+import Cache from "./helperComponents/Cache";
+import Block from "./helperComponents/Block";
+import Arrow from "./helperComponents/Arrow";
 
 export default function Home() {
   const [word, setWord] = useState("");
@@ -17,13 +20,32 @@ export default function Home() {
         </div>
 
         {/* Main View */}
-        <div className="w-[70vw] h-full bg-gray-300 flex flex-col justify-between overflow-hidden">
-          <div>
-            <MainMemory/>
+        <div className="w-[70vw] h-full bg-gray-300 flex flex-col justify-between overflow-auto">
+          <div className="w-full h-[10vh] p-2 flex justify-between items-center text-white font-bold text-xl">
+            <h1 className=" px-10">Cache Simulator</h1>
+            <div className="relative inline-block group px-10 rounded bg-green-500">
+              <button>
+                <h1 className="cursor-pointer hover:text-gray-400">
+                  view
+                </h1>
+              </button>
+              <div className="absolute top-7 left-0 w-full bg-green-500 rounded">
+                <button className="w-full block hidden group-hover:block bg-green-500 cursor-pointer hover:bg-blue-400">
+                  <h1>Step by Step</h1>
+                </button>
+                <button className="w-full block hidden group-hover:block bg-green-500 cursor-pointer hover:bg-blue-400">
+                  <h1>Final BSA</h1>
+                </button>
+              </div>
+            </div>
           </div>
           {/* Simulator */}
-          <div className="flex-1 p-4 overflow-auto text-white">
-            {word}
+          <div className="flex justify-between items-center gap-2 p-4">
+            <Cache address={""} value={""} numSet={4}/>
+            <Arrow/>
+            <Block blockNum={1} numWord={4}/>
+            <Arrow/>
+            <MainMemory/>
           </div>
 
           {/* Console */}
