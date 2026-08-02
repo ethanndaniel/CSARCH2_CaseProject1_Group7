3b3c8dda52ffaac9142982b809205b8e83a2e9f2
naynilenipse <pebiswaggot@protonmail.com>
2026-08-02 13:31:00 +0800

    Add files via upload

--- simulation/src/app/cache/CacheSimulator.js
+++ simulation/src/app/cache/CacheSimulator.js
@@ -8,6 +8,11 @@ import {
 }
 from './ReplacementPolicy.js';
 
+import {
+    ReadPolicy
+}
+from './ReadPolicy.js';
+
 //values derived from sir rog's cache memory lessons
 export const Timing = {
     cacheAccessTime: 1, //1 tau second for cache access
@@ -19,6 +24,7 @@ export function createSimulator(cache, replacementPolicy) {
     let accessCount = 0;
     let hitCount = 0;
     let missCount = 0;
+    let log = [];
 
     function accessMemoryBlock(memoryBlock) {
         accessCount++;
@@ -31,18 +37,42 @@ export function createSimulator(cache, replacementPolicy) {
         if (hitway !== -1) {
             hitCount++;
             cacheSet.ways[hitway].lastAccess = accessCount;
+
+            log.push(traceLogs(hitway, true, null));
+
             return true; //hit
         }
         else {
             missCount++;
             const victimWay = selectVictim(cacheSet, replacementPolicy);
+
+            const evictedBlock = cacheSet.ways[victimWay].valid ? cacheSet.ways[victimWay].memoryBlock : null;
+
             cacheSet.ways[victimWay].valid = true;
             cacheSet.ways[victimWay].tag = tag;
             cacheSet.ways[victimWay].lastAccess = accessCount;
+
             cacheSet.ways[victimWay].memoryBlock = memoryBlock;
+
+            log.push(traceLogs(victimWay, false, evictedBlock));
+
             return false; //miss
         }
+
+        function traceLogs(way, isHit, evictedBlock) {
+            return {
+                accessNumber: accessCount,
+                memoryBlock: memoryBlock,
+                setIndex: index,
+                tag: tag,
+                way: way,
+                hit: isHit,
+                evictedBlock: evictedBlock
+            };
+        }
+        
     }
+
 
     function getStats() {
         if (accessCount === 0) {
@@ -72,19 +102,28 @@ export function createSimulator(cache, replacementPolicy) {
     
     //formula derived from sir rog's cache memory lessons
     function calculateAMAT(hitRate, cacheAccessTime, memoryAccessTimePerWord) {
-            const M = 1 + (memoryAccessTimePerWord * cache.blockSize) + 1;
-            const AMAT = (hitRate * cacheAccessTime) + ((1 - hitRate) * M);
-            return AMAT;
+        let M;
+        
+        if (cache.readPolicy == ReadPolicy.LOAD_THROUGH) {
+            M = 1 + memoryAccessTimePerWord;
+        }
+        else {
+            M = 1 + (memoryAccessTimePerWord * cache.blockSize) + 1;
+        }
+            
+        const AMAT = (hitRate * cacheAccessTime) + ((1 - hitRate) * M);
+        return AMAT;
     }
     
     
-    function calculateTMAT(hitRate, cacheAccessTime, memoryAccessTimePerWord) {
-            const TMAT = calculateAMAT(hitRate, cacheAccessTime, memoryAccessTimePerWord) * accessCount; 
-            return TMAT;
+    function calculateTMAT(hitRate, cacheAccessTime, memoryAccessTimePerWord) {            
+        const TMAT = calculateAMAT(hitRate, cacheAccessTime, memoryAccessTimePerWord) * accessCount; 
+        return TMAT;
     }
 
     return {
         accessMemoryBlock,
-        getStats
+        getStats,
+        getLog: () => log
     };
 }
